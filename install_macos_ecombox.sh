8f2544f2de6091da8313b853f5c61ddb5821b4f3
ChristelleThiry <christelle.trucchi@gmail.com>
2019-11-08 13:59:30 +0100

    Update install_macos_ecombox.sh

--- install_macos_ecombox.sh
+++ install_macos_ecombox.sh
@@ -47,12 +47,12 @@ echo -e "Récupération des paramètres du proxy s'ils existent"
 
 IS_PROXY_ENABLED = networksetup -getwebproxy Ethernet | grep ^Enabled: | awk {'print $2'}
 SERVICE = "Ethernet"
-if [IS_PROXY_ENABLED == ""]; then
+if ["$IS_PROXY_ENABLED" == ""]; then
     IS_PROXY_ENABLED = networksetup -getwebproxy Wi-Fi | grep ^Enabled: | awk {'print $2'}
     SERVICE = "Wi-Fi"
 fi
 
-if [IS_PROXY_ENABLED == "Yes"]; then
+if ["$IS_PROXY_ENABLED" == "Yes"]; then
     ADRESSE_PROXY = networksetup -getwebproxy $SERVICE | awk {'print $2'} | awk {'getline l2; getline l3; print l2":"l3'} | head -n 1
 fi 
 
